a2b35b21ca43ce92501a6c77dbe2aa8cd25aaa36
Daniel Ohlsson <dohlsson89@gmail.com>
2018-01-29 21:30:40 +0100

    Added user api routes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 'use strict';
-const express = require('express');                                                            
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const api = require('./api');
 const app = express();
 
-app.get('/', function(req, res){                                                               
-  res.sendFile('index.html', { root: __dirname + "/web" } );
-});
-   
+// Serve static content from src/web
 app.use(express.static('src/web'));
-    
+
+// parse application/json
+app.use(bodyParser.json());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/api', api);
+
 app.listen(3000, function () {
   console.log('Listening on http://localhost:3000');
 });
