a7ad25e1383122923913e31e4925d14436deca84
Daniel Ohlsson <dohlsson89@gmail.com>
2018-01-30 22:17:20 +0100

    tweaks to the elo algorithm

--- src/elo.js
+++ src/elo.js
@@ -1,19 +1,19 @@
 'use strict';
 
-// replace with k-value
-const stake = 10;
+// a commonly used constant
+const kfactor = 32;
 
 function match(winner, loser) {
   const prob = probabilityOfWinning(winner, loser);
-  const winnerGain = Math.round((1 - prob) * boost(winner) * stake);
-  const loserGain = -Math.round((1 - prob) * boost(loser) * stake);
+  const winnerGain = Math.round((1 - prob) * boost(winner) * kfactor);
+  const loserGain = -Math.round((1 - prob) * boost(loser) * kfactor);
 
   return { winnerGain, loserGain };
 }
 
 function probabilityOfWinning(p1, p2) {
   const diff = p1.rating - p2.rating;
-  const probability = 1 / (1 + Math.pow(10, -diff/500));
+  const probability = 1 / (1 + Math.pow(10, -diff/400));
 
   const pretty = Math.round(probability*1000)/10;
   console.log('probability %s win over %s = %d%%', p1.username, p2.username, pretty);
@@ -21,6 +21,7 @@ function probabilityOfWinning(p1, p2) {
   return probability;
 }
 
+// boost function to make new players elo adapt quicker
 function boost(p) {
   if (p.matches < 5) {
     return 2;
