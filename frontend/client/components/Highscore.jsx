f809aaf81a5be566cb4752d355148509496cfc30
FilipHarald <filip.harald@gmail.com>
2018-01-30 21:37:58 +0100

    added highscore

---
+++ frontend/client/components/Highscore.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class Highscore extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      users: null
+    };
+  }
+  render () {
+    let userList;
+    if (this.state.users) {
+      const listItems = this.state.users.map(user => {
+        return <li key={user._id}>{user.username} ({user.rating} rating)</li>;
+      });
+      userList = <ol>{listItems}</ol>;
+    }
+    return (
+      <div>
+        <h2>Higscore</h2>
+        {userList || 'loading users...'}
+      </div>);
+  }
+  componentDidMount () {
+    axios.get('http://localhost:3000/api/users', {
+      headers: { 'Access-Control-Allow-Origin': '*' }
+    })
+    .then(response => {
+      this.setState({users: response.data.sort((a, b) => {
+        return b.rating - a.rating;
+      })});
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  }
+}
