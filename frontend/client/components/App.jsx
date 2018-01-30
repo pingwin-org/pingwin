74d961d4bb9e5c644735fb3a620141eefabfbc9a
FilipHarald <filip.harald@gmail.com>
2018-01-30 21:37:58 +0100

    added highscore

--- frontend/client/components/App.jsx
+++ frontend/client/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import logo from '../img/penguin.png'
+import logo from '../img/penguin.png';
+import Highscore from './Highscore.jsx';
 
 export default class App extends React.Component {
   render () {
     return (
       <div style={{textAlign: 'center'}}>
         <h1>Hello Pingwinner!</h1>
-        <img src={logo} />
+        <img src={logo} height='200' />
+        <Highscore />
       </div>);
   }
 }
