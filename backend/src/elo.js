c6221191cc27972d9b0d95f8f67aeb4100fc1d04
Daniel Ohlsson <daniel.ohlsson@sensative.com>
2018-01-31 18:07:27 +0100

    Modified elo boost and k-factor comment

--- backend/src/elo.js
+++ backend/src/elo.js
@@ -1,10 +1,13 @@
 'use strict';
 
-// a commonly used constant
+// The k-factor is a constant used for determining how much rating can fluctuate
+// this is the highest value one can gain/lose in a single match (disregarding boost)
+// for evenly matched players the gain is roughly equal to half the k-factor
 const kfactor = 32;
 
 function match(winner, loser) {
   const prob = probabilityOfWinning(winner, loser);
+
   const winnerGain = Math.round((1 - prob) * boost(winner) * kfactor);
   const loserGain = -Math.round((1 - prob) * boost(loser) * kfactor);
 
@@ -23,7 +26,7 @@ function probabilityOfWinning(p1, p2) {
 
 // boost function to make new players elo adapt quicker
 function boost(p) {
-  if (p.matches < 5) {
+  if (p.matches < 10) {
     return 2;
   } else {
     return 1;
