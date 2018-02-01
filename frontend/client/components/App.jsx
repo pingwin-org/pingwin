73b1095a8e95a9d25f894d647d5d4c1f78652423
FilipHarald <filip.harald@gmail.com>
2018-02-01 22:02:41 +0100

    ugly images

--- frontend/client/components/App.jsx
+++ frontend/client/components/App.jsx
@@ -1,16 +1,39 @@
 import React from 'react';
-import logo from '../img/pingwin.png';
+import penguin from '../img/penguin.png';
+import pingwin from '../img/pingwin.png';
+import racket from '../img/racket.png';
+import rackets from '../img/rackets.png';
 import UserList from './UserList.jsx';
 import MatchList from './MatchList.jsx';
 import AddUser from './AddUser.jsx';
 import AddMatch from './AddMatch.jsx';
 
 export default class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      usersImg: penguin,
+      matchesImg: racket
+    };
+    this.usersHover = this.usersHover.bind(this);
+    this.matchesHover = this.matchesHover.bind(this);
+  }
+  usersHover (e) {
+    this.setState(prevState => ({
+      usersImg: prevState.usersImg === penguin ? pingwin : penguin
+    }));
+  }
+  matchesHover (e) {
+    this.setState(prevState => ({
+      matchesImg: this.state.matchesImg === racket ? rackets : racket
+    }));
+  }
   render () {
     return (
       <div style={{textAlign: 'center'}}>
         <h1>Hello Pingwinner!</h1>
-        <img src={logo} height='200' />
+        <img src={this.state.usersImg} onMouseEnter={this.usersHover} onMouseLeave={this.usersHover} height='200' width='200' />
+        <img src={this.state.matchesImg} onMouseEnter={this.matchesHover} onMouseLeave={this.matchesHover} height='200' width='200' />
         <UserList />
         <AddUser />
         <MatchList />
