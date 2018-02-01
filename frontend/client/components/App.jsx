7192b27708c8b84b29b299d3b6b897c26d349838
Daniel Ohlsson <dohlsson89@gmail.com>
2018-02-01 19:23:06 +0100

    Added matchlist

--- frontend/client/components/App.jsx
+++ frontend/client/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from '../img/pingwin.png';
 import UserList from './UserList.jsx';
+import MatchList from './MatchList.jsx';
 import AddUser from './AddUser.jsx';
 
 export default class App extends React.Component {
@@ -11,6 +12,7 @@ export default class App extends React.Component {
         <img src={logo} height='200' />
         <UserList />
         <AddUser />
+        <MatchList />
       </div>);
   }
 }
