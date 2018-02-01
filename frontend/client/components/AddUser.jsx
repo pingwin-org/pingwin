8430a1144b9cf72190674f3cac493fbae8051c3e
Daniel Ohlsson <dohlsson89@gmail.com>
2018-02-01 19:10:57 +0100

    Add user from frontend

---
+++ frontend/client/components/AddUser.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class Highscore extends React.Component {
+  render () {
+    return (
+      <div>
+        <h3>Add User</h3>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <div>
+            <label>Username</label>
+            <input type="text" ref="username" />
+          </div>
+          <div>
+            <label>Pin</label>
+            <input type="password" ref="pin" />
+          </div>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>);
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.refs.username.value);
+    console.log(this.refs.pin.value);
+    axios.post('http://localhost:3000/api/users', {
+      username: this.refs.username.value,
+      pin: this.refs.pin.value
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+}
+
--- frontend/client/components/App.jsx
+++ frontend/client/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from '../img/pingwin.png';
 import Highscore from './Highscore.jsx';
+import AddUser from './AddUser.jsx';
 
 export default class App extends React.Component {
   render () {
@@ -9,6 +10,7 @@ export default class App extends React.Component {
         <h1>Hello Pingwinner!</h1>
         <img src={logo} height='200' />
         <Highscore />
+        <AddUser />
       </div>);
   }
 }
