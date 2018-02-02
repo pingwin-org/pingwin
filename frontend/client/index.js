e940a54f046df53b47abfd8fe458f54e46364781
Daniel Ohlsson <dohlsson89@gmail.com>
2018-02-02 21:38:51 +0100

    Sexy beige background

--- frontend/client/index.js
+++ frontend/client/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 import 'bootstrap/dist/css/bootstrap.css';
+import './css/App.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
