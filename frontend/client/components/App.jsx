b81639ba96d22fd271a2a037d78f5e95e47f2658
FilipHarald <filip.harald@gmail.com>
2018-02-03 15:51:31 +0100

    Added proper Bootstrap override and SASS! :)

--- frontend/client/components/App.jsx
+++ frontend/client/components/App.jsx
@@ -7,6 +7,7 @@ import UserList from './UserList.jsx';
 import MatchList from './MatchList.jsx';
 import AddUser from './AddUser.jsx';
 import AddMatch from './AddMatch.jsx';
+import { Well, Grid } from 'react-bootstrap';
 
 export default class App extends React.Component {
   constructor (props) {
@@ -30,14 +31,16 @@ export default class App extends React.Component {
   }
   render () {
     return (
-      <div style={{textAlign: 'center'}}>
-        <h1>Hello Pingwinner!</h1>
+      <Grid>
+        <Well>
+          <h1>Hello Pingwinner!</h1>
+        </Well>
         <img src={this.state.usersImg} onMouseEnter={this.usersHover} onMouseLeave={this.usersHover} height='200' width='200' />
         <img src={this.state.matchesImg} onMouseEnter={this.matchesHover} onMouseLeave={this.matchesHover} height='200' width='200' />
         <UserList />
         <AddUser />
         <MatchList />
         <AddMatch />
-      </div>);
+      </Grid>);
   }
 }
