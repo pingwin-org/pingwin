c9608cde41b9a953e7b173d81c04828ca41bb6df
FilipHarald <filip.harald@gmail.com>
2018-02-03 19:09:36 +0100

    added code for navbar, but it's not working :(

--- frontend/client/components/App.jsx
+++ frontend/client/components/App.jsx
@@ -1,34 +1,18 @@
 import React from 'react';
-import penguin from '../img/penguin.png';
-import racket from '../img/racket.png';
+import Menu from './Menu.jsx';
+import Selector from './Selector.jsx';
 import UserList from './UserList.jsx';
 import MatchList from './MatchList.jsx';
 import AddUser from './AddUser.jsx';
 import AddMatch from './AddMatch.jsx';
-import { Grid, Row, Col, Image } from 'react-bootstrap';
+import { Grid } from 'react-bootstrap';
 
 export default class App extends React.Component {
   render () {
     return (
       <Grid>
-        <Grid>
-          <Row>
-            <Col className='container col-6 bg'>
-              <Image src={penguin} />
-              <div className='overlay'>
-                <h2>Users</h2>
-                <p>Check out the rating of all other pingwinners. And more imporantantly who's in the toplist.</p>
-              </div>
-            </Col>
-            <Col className='container col-6 bg'>
-              <Image src={racket} />
-              <div className='overlay'>
-                <h2>Play!</h2>
-                <p>Do you have another pingwinner to challenge in a game of table tennis?</p>
-              </div>
-            </Col>
-          </Row>
-        </Grid>
+        <Menu />
+        <Selector />
         <UserList />
         <AddUser />
         <MatchList />
