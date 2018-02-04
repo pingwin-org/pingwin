f48d66a4bf2dda3dbac2c31cc519af20ff3367d2
FilipHarald <filip.harald@gmail.com>
2018-02-04 18:40:33 +0100

    added footer

---
+++ frontend/client/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import git from '../img/github.png';
+import { Card, CardFooter } from 'reactstrap';
+
+export default class Footer extends React.Component {
+  render () {
+    return (
+      <footer>
+        <Card>
+          <CardFooter style={{'text-align': 'center'}}>
+            <p>
+              <a href='https://github.com/DOhlsson/pingwin'>
+                <img src={git} /> All your stars are belong to us!
+              </a>
+            </p>
+          </CardFooter>
+        </Card>
+      </footer>);
+  }
+}
