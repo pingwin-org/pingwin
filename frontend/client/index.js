3760ce73a54d327cc1fb0f09f0da257aefba7ba8
Daniel Ohlsson <dohlsson89@gmail.com>
2018-02-06 21:23:18 +0100

    Added redux stub

--- frontend/client/index.js
+++ frontend/client/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
+
 import App from './components/App.jsx';
 import './scss/main.scss';
 import penguin from './img/penguin.png';
 import Favicon from 'react-favicon';
 
+const store = createStore(reducers);
+
 ReactDOM.render(
-  <div>
-    <Favicon url={penguin} />
+  <Provider store={store}>
+    {/*<Favicon url={penguin} />*/}
     <App />
-  </div>
+  </Provider>
   , document.getElementById('root'));
