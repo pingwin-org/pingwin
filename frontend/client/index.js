24f2b1c152b4c2716743f37bb38ae430e84d7881
FilipHarald <filip.harald@gmail.com>
2018-02-07 20:34:11 +0100

    finally react-router is installed. Now the fun begins...

--- frontend/client/index.js
+++ frontend/client/index.js
@@ -1,19 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import reducers from './reducers';
+import { Route } from 'react-router';
+import createHistory from 'history/createBrowserHistory';
+import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux';
+import Favicon from 'react-favicon';
 
+import reducers from './reducers';
 import App from './components/App.jsx';
 import './scss/main.scss';
 import penguin from './img/penguin.png';
-import Favicon from 'react-favicon';
 
-const store = createStore(reducers);
+const history = createHistory();
+const middleware = routerMiddleware(history);
+const store = createStore(
+  combineReducers({
+    ...reducers,
+    router: routerReducer
+  }),
+  applyMiddleware(middleware)
+);
 
 ReactDOM.render(
   <Provider store={store}>
-    {/*<Favicon url={penguin} />*/}
-    <App />
+    <ConnectedRouter history={history}>
+      <div>
+        <Route path='/' component={App} />
+        <Favicon url={penguin} />
+      </div>
+    </ConnectedRouter>
   </Provider>
   , document.getElementById('root'));
--- frontend/client/reducers/matches.js
+++ frontend/client/reducers/matches.js
@@ -1,6 +1,6 @@
 const matches = (state = {}, action) => {
   console.log('matches reducer', state, action);
   return state;
-}
+};
 
 export default matches;
