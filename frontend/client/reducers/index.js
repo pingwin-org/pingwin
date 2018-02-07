07e8c77314e4583da9a53814fde233dc7d25699e
FilipHarald <filip.harald@gmail.com>
2018-02-07 20:34:11 +0100

    finally react-router is installed. Now the fun begins...

--- frontend/client/reducers/index.js
+++ frontend/client/reducers/index.js
@@ -1,10 +1,9 @@
-import { combineReducers } from 'redux';
 import users from './users';
 import matches from './matches';
 
-const reducers = combineReducers({
+const reducers = [
   users,
   matches
-});
+];
 
 export default reducers;
