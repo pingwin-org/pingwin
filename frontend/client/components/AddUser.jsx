956d77e99b8510fa32f6186e70f3de28832dbcbf
Daniel Ohlsson <dohlsson89@gmail.com>
2018-02-18 00:48:46 +0100

    Mini cleanup

--- frontend/client/components/AddUser.jsx
+++ frontend/client/components/AddUser.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios';
 import { Form, FormGroup, Label, Input, Button, Alert } from 'reactstrap';
 import { connect } from 'react-redux'
 import { addUser } from '../actions'
